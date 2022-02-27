0d77f22fc069c4631c9978597606439101cca3c1
Kareem AbuRijela <39139830+KareemAbuRejila@users.noreply.github.com>
2022-02-27 13:38:25 -0600

    Initial commit

---
+++ app/src/main/java/edu/miu/cs473de/w2a3l4/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package edu.miu.cs473de.w2a3l4.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.miu.cs473de.w2a3l4.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/edu/miu/cs473de/w2a3l4/models/AndroidVersionModel.kt
@@ -0,0 +1,6 @@
+package edu.miu.cs473de.w2a3l4.models
+
+data class AndroidVersionModel (
+    val ver:String,
+    val code:String
+        )
